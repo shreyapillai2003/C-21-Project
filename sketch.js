9e981ad3ee4853341052491a3490ebeb947b23ad
shreyapillai2003 <84273883+shreyapillai2003@users.noreply.github.com>
2021-09-18 18:01:56 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,12 +1,8 @@
 var wall, thickness;
 var bullet,speed, weight;
-var damage;
-
-
-
 function setup() {
   createCanvas(1600,400);
-
+  thickness= random(22,83);
   speed= random(223,321)
   weight= random(30,52)
   bullet = createSprite(50, 200, 50, 10);
@@ -14,64 +10,49 @@ function setup() {
   bullet.velocityX= speed;
   wall= createSprite(1200,200,thickness,height/2);
   wall.shapeColour=color(80,80,80)
-
-  thickness= random(22,83);
-
+ // thickness= random(22,83);
 }
-
 function draw() {
-  background(0,0,255);
+  background(0);
   
-  // if(hasCollided(bullet,wall)) {
-  //   bullet.velocityX=0;
-  //   damage= 0.5 *width *speed *speed/ (thickness *thickness *thickness);
+  if(hasCollided(bullet,wall)) {
+    bullet.velocityX=0;
+    var damage= 0.5 *width *speed *speed/ (thickness *thickness *thickness);
 
-  //   if(damage>10) {
-  //     wall.shapeColor= color(255,0,0)
-  //   }
+    if(damage>10) {
+      wall.shapeColor= color(255,0,0)
+    }
   
-  // if(damage<10) {
-  //  wall.shapeColor= color(0,255,0); c
-  // }
-  
-  // }
-  
-  
-   hasCollided();
+  if(damage<10) {
+   wall.shapeColor= color(0,255,0); 
+  }
+   
+}
+   //hasCollided();
   drawSprites(); 
-
 }
 
 function hasCollided(bullet, wall) {
       bulletRightEdge= bullet.x+ bullet.width;
       wallLeftEdge= wall.x;
-
       if(bulletRightEdge>=wallLeftEdge) {
         return true;
       }
       return false;
 
-      if(hasCollided(bullet,wall)) {
-        bullet.velocityX=0;
-        damage= 0.5 *width *speed *speed/ (thickness *thickness *thickness);
+      // if(hasCollided(bullet,wall)) {
+      //   bullet.velocityX=0;
+      //   damage= 0.5 *width *speed *speed/ (thickness *thickness *thickness);
     
-        if(damage>10) {
-          wall.shapeColor= color(255,0,0)
-        }
+      //   if(damage>10) {
+      //     wall.shapeColor= color(255,0,0)
+      //   }
       
-      if(damage<10) {
-       wall.shapeColor= color(0,255,0); 
-      }
+      // if(damage<10) {
+      //  wall.shapeColor= color(0,255,0); 
+      // }
       
-      }
+     // }
       
 
 }
-
-
-
-
-
-
-
-
